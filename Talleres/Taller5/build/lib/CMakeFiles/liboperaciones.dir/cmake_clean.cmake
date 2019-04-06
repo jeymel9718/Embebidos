2e9bf04fb4b5c30460f74e0bf9fdee81785ea9ba
jeymel9718 <jey9608@hotmail.com>
2019-04-06 16:26:19 -0600

    Taller5

---
+++ Talleres/Taller5/build/lib/CMakeFiles/liboperaciones.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/liboperaciones.dir/operaciones.c.o"
+  "libliboperaciones.pdb"
+  "libliboperaciones.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/liboperaciones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
